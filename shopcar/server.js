d2bbf7f5c4ac1e62e82ca7b3dc41f21cd2c92168
Kunal <patelkr07@gmail.com>
2019-08-12 22:36:48 -0500

    fixing server.js page

--- shopcar/server.js
+++ shopcar/server.js
@@ -12,7 +12,7 @@ if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
 }
 
-app.use("/api", apiRoutes);
+// app.use("/api", apiRoutes);
 
 app.get("*", function(req, res) {
     res.sendFile(path.join(__dirname, "./client/build/index.html"));
